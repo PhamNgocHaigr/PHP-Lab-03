3100aaec3413b145e873cf7af115382b8f793eb8
phamngochaigr <phamngochaimmo@gmail.com>
2024-03-20 16:34:32 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+</div>
+<footer>
+    &#169; 2024 - Ngoc Hai - Student of Infomation Technology (HUTECH)
+</footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project training - website bán hàng</title>
+</head>
+<body>
+    <div id="wrapper">
+    <h2>Project training - Xây dựng website bán hàng</h2>
